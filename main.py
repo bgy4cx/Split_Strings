17cc95a761b14c146ea29c3c83556229bcbb95f0
György Végh <gyorgy.vegh.job@gmail.com>
2022-06-07 11:30:27 +0200

    SonarCloud

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-def solution(inputText):
-    inputText += '_'
-    return [(inputText[x-1] + inputText[x]) for x in range(len(inputText)) if (x + 1) % 2 == 0]
+def solution(Input_str):
+    Input_str += '_'
+    return [(Input_str[x-1] + Input_str[x]) for x in range(len(Input_str)) if (x + 1) % 2 == 0]
